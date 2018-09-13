f150701e515b0fedc188ed4b24987ade6f832902
Yukizakura <sakurazaki.rikka@gmail.com>
2018-09-13 16:40:07 +0800

    Docomoアプリを消すコード adb必須

---
+++ so-01j_appdelete.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+# As a reference to erase docomo application of docomo xperia
+# "pm disable-user" disappears after executing "pm uninstall -k --user 0"
+
+
+adb shell pm disable-user com.nttdocomo.android.wipe
+adb shell pm disable-user com.nttdocomo.android.anmane2
+adb shell pm disable-user com.nttdocomo.android.databackup
+adb shell pm disable-user com.nttdocomo.android.screenlockservice
+adb shell pm disable-user com.nttdocomo.android.felicaremotelock
+adb shell pm disable-user com.nttdocomo.android.iconcier
+adb shell pm disable-user com.rsupport.rsperm.ntt
+adb shell pm disable-user com.nttdocomo.android.dmenu2
+adb shell pm disable-user com.nttdocomo.android.schedulememo
+adb shell pm disable-user com.nttdocomo.android.pf.dcmwappush
+adb shell pm disable-user com.nttdocomo.android.mascot
+adb shell pm disable-user jp.co.nttdocomo.anshinmode
+adb shell pm disable-user com.nttdocomo.android.iconcier_contents
+adb shell pm disable-user com.nttdocomo.android.docomoset
+adb shell pm disable-user com.nttdocomo.android.applicationmanager
+adb shell pm disable-user com.nttdocomo.android.apnsw
+adb shell pm disable-user com.nttdocomo.android.dhome
+adb shell pm disable-user com.nttdocomo.android.atf
+adb shell pm disable-user com.nttdocomo.android.osv
+adb shell pm disable-user com.nttdocomo.android.bugreport
+adb shell pm disable-user com.nttdocomo.android.tapandpay
+adb shell pm disable-user com.nttdocomo.android.pf.dcmippushaggregator
+adb shell pm disable-user com.nttdocomo.android.gs.utility_sha2
+adb shell pm disable-user com.nttdocomo.android.phonemotion
+adb shell pm disable-user com.nttdocomo.android.cloudset
+adb shell pm disable-user jp.co.nttdocomo.bridgelauncher
+adb shell pm disable-user jp.co.nttdocomo.lcsappsub
+adb shell pm disable-user com.nttdocomo.android.accountauthenticator
+adb shell pm disable-user com.nttdocomo.android.idmanager
+adb shell pm disable-user com.nttdocomo.android.voiceeditor
+adb shell pm disable-user com.nttdocomo.android.mediaplayer
+adb shell pm disable-user com.nttdocomo.android.sdcardbackup
+adb shell pm disable-user com.nttdocomo.android.accountwipe
+adb shell pm disable-user com.nttdocomo.android.devicehelp
+adb shell pm disable-user com.nttdocomo.android.initialization
+adb shell pm disable-user jp.co.nttdocomo.carriermail
+adb shell pm disable-user com.nttdocomo.android.areamail
+adb shell pm disable-user com.nttdocomo.android.remotelock
+
+
+
+
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.store
+adb shell pm uninstall -k --user 0 jp.co.nttdocomo.lcsappsub
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.atf
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.cloudset
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.mediaplayer
+adb shell pm uninstall -k --user 0 com.facebook.system
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.facebook.katana
+adb shell pm disable-user com.nttdocomo.android.iconcier_contents
+adb shell pm disable-user com.nttdocomo.android.iconcier
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.iconcier_contents
+adb shell pm uninstall -k --user 0 com.nttdocomo.android.iconcier
+adb shell pm disable-user jp.co.nttdocomo.lcsapp
+adb shell pm disable-user com.nttdocomo.android.mascot
+adb shell pm uninstall -k –user 0 com.nttdocomo.android.mascot
+adb shell pm Code to erase docomo application of docomo xperia
+Disable-user disappears after uninstalling -k –user 0 com.nttdocomo.android.dcmvoicerecognition
+adb shell pm uninstall -k –user 0 jp.co.nttdocomo.lcsapp
+adb shell pm uninstall -k –user 0 jp.co.nttdocomo.carriermail
+adb shell pm uninstall -k –user 0 com.nttdocomo.android.atf
+
+adb shell pm uninstall -k –user 0 com.android.contacts
+adb shell pm uninstall -k –user 0 com.sonymobile.entrance
